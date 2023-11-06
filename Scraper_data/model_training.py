80d2a1a0437288915487ab0d87b24afdfdfcd447
kbattah <kenzabattah@gmail.com>
2023-11-06 01:57:18 -0500

    Delete Scraper_data/model_training.py
    Useless, put code in data_cleaner

--- Scraper_data/model_training.py
+++
@@ -1,9 +0,0 @@
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn import linear_model
-from sklearn.ensemble import AdaBoostClassifier
-
-X_train, X_test, y_train, y_test = train_test_split(data.iloc[:, 1:3], data['Category'], random_state = 0)
-X_train_title_features = tfidf_title.transform(X_train['Title']).toarray()
-X_train_desc_features = tfidf_desc.transform(X_train['Description']).toarray()
-features = np.concatenate([X_train_title_features, X_train_desc_features], axis=1)
